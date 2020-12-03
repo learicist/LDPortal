1cab3a0e88e220f87ba82b0c45cf20a74d2444e8
Field <fpalmer@2u.com>
2020-12-02 22:51:48 -0700

    "styling to match L&D site"

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 $(document).ready(function () {
+   //    let pass = "1want2teach!";
+   let pass = "letmein123!";
+   //console.log(pass);
 
-	
-	let pass = "1want2teach!";
-	//console.log(pass);
-	
-	let users = ["mleary@2u.com", ""];
-	let userEntry = $('#email').val();
-	
-	$('#submit').click(function () {
-		if (users.includes(userEntry) && $('#pass').val() === pass) {
-			alert("you're in");
-		}
-	});
+   let users = ["mleary@2u.com", "fpalmer@2u.com"];
+   let userEntry = $("#email").val();
+
+   $("#submit").click(function () {
+      if (users.includes(userEntry) && $("#pass").val() === pass) {
+         alert("yup");
+      } else {
+         alert("somethings up");
+      }
+   });
 });
-
